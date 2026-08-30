a11eae5a0d9213ac378ebc7607c9f170a35a47d0
PLUT0SKI <patriciomaya72@gmail.com>
2026-08-30 17:14:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const PRECIO_DEPOSITO = 100;
 // Conjunto completo = camisa + pantalón/short
 const MULTIPLICADOR_CAMISA = 1;
 const MULTIPLICADOR_PANTALON = 1;
-const MULTIPLICADOR_CONJUNTO = 2;
+const MULTIPLICADOR_CONJUNTO = 1.5;
 
 // ==========================================
 // REACTION ROLES
