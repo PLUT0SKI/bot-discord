dd572ecba46088ee8d387dc9e64dd0bab024b917
PLUT0SKI <patriciomaya72@gmail.com>
2026-08-31 14:00:32 -0600

    Fix: agregar alias deploy.commands.js para registro de comandos

---
+++ deploy.commands.js
@@ -0,0 +1,3 @@
+// Alias para mantener compatibilidad con el comando `node deploy.commands.js`.
+// El archivo principal para registrar los comandos es deploy-commands.js.
+require('./deploy-commands.js');
