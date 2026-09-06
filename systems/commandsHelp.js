55218c65acff3675f7c04014d8fdf012a3be2cca
PLUT0SKI <patriciomaya72@gmail.com>
2026-09-06 17:45:37 -0600

    Añadir /name al listado de comandos

--- systems/commandsHelp.js
+++ systems/commandsHelp.js
@@ -22,6 +22,10 @@ const COMANDOS = [
     comandos: [['`/invites`', 'Consulta cuántas invitaciones tiene un usuario.']]
   },
   {
+    categoria: '🔎 Nombres',
+    comandos: [['`/name`', 'Genera nombres de usuario de 3 caracteres que no aparecen en este servidor.']]
+  },
+  {
     categoria: '🛠️ Administración',
     comandos: [
       ['`/clear`', 'Elimina mensajes del canal.'],
