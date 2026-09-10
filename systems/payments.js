7a13e43e346890f57078f2a3d6ccdf6bd54920e3
PLUT0SKI <patriciomaya72@gmail.com>
2026-09-09 19:55:11 -0600

    Fix /pagos response to hide command line

--- systems/payments.js
+++ systems/payments.js
@@ -11,36 +11,31 @@ module.exports = (client) => {
         .setTitle('💳 MÉTODOS DE PAGO')
         .setDescription(
           'Aceptamos los siguientes métodos de pago:\n\n' +
-                        
-
           '<:remitly:1547413850683609178> **REMITLY**\n' +
           '> Pagos internacionales de forma rápida y segura.\n' +
           '> Ideal para pagos realizados desde otro país.\n\n' +
-
-
           '<:oxxo:1547413887400288256> **DEPÓSITO EN OXXO**\n' +
           '> Realiza tu pago en efectivo desde cualquier sucursal OXXO.\n' +
           '> Disponible para pagos dentro de México.\n\n' +
-
-
           '<:transfe:1547413933290160148> **TRANSFERENCIA BANCARIA**\n' +
           '> Realiza una transferencia directamente desde tu banco.\n' +
           '> Disponible para pagos nacionales.\n\n' +
-
-          '━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n'+
-
+          '━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' +
           '<:buy:1547419630472011807> **¿QUIERES REALIZAR UNA COMPRA?**\n' +
-          '> Abre un <#1357832842561978505> y nuestro equipo te proporcionará los datos necesarios para completar tu pago.' 
-          
+          '> Abre un <#1357832842561978505> y nuestro equipo te proporcionará los datos necesarios para completar tu pago.'
         )
-
         .setFooter({
           text: 'Zutaniza • Tu tienda de confianza'
         })
         .setTimestamp();
 
-      await interaction.reply({
+      await interaction.channel.send({
         embeds: [embed]
+      });
+
+      await interaction.reply({
+        content: '\u200b',
+        ephemeral: true
       });
 
     } catch (error) {
