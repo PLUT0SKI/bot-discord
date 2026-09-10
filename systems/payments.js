ca0dd7bc4ef78efcc739e99e7e41499243ca9bf8
PLUT0SKI <patriciomaya72@gmail.com>
2026-09-09 19:51:46 -0600

    Fix formatting in payment message prompt

--- systems/payments.js
+++ systems/payments.js
@@ -30,7 +30,7 @@ module.exports = (client) => {
           '━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n'+
 
           '<:buy:1547419630472011807> **¿QUIERES REALIZAR UNA COMPRA?**\n' +
-          '> Abre un <#1357832842561978505> y nuestro equipo te proporcionará los datos necesarios para completar tu pago.' +
+          '> Abre un <#1357832842561978505> y nuestro equipo te proporcionará los datos necesarios para completar tu pago.' 
           
         )
 
